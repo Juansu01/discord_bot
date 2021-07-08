b6f72a15944ae6b555ed662ebfc977341da9f049
Juansu01 <thejuank0@hotmail.com>
2021-07-08 15:54:56 -0500

    Adding my first discord bot files

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Sister is online!');
+});
+
+client.login('ODYyNzg2MTg4NjQ1NjI5OTcz.YOdaQQ.amVOPuOnc4h8dXJDQgSETNzuIao');
